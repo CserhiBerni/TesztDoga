3fddcd8e94d4ed2641b1f27f7890d6f1903150b7
Petrik Diak <diak@petrik.hu>
2023-12-18 12:21:15 +0100

    Added functionality with js

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 import './style.css'
 
+let numberList = [];
 
+window.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("addNumber").addEventListener("click", () => {
+    let number = parseInt(document.getElementById("input").value);
+    numberList.push(number);
+    document.getElementById("list").innerHTML = "";
+
+    for (let i = 0; i < numberList.length; i++) {
+      let li = document.createElement("li");
+      li.textContent = numberList[i];
+      document.getElementById("list").appendChild(li);
+    }
+  });
+});
