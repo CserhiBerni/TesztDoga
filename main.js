2111e1c1bf3f2f38833e52e48d00af81e7bac8eb
Petrik Diak <diak@petrik.hu>
2023-12-18 12:31:56 +0100

    created showMinimum button with functionality

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ window.addEventListener("DOMContentLoaded", () => {
     let number = parseInt(document.getElementById("input").value);
     numberList.push(number);
     document.getElementById("list").innerHTML = "";
+    document.getElementById("minimum").innerHTML = "";
 
     for (let i = 0; i < numberList.length; i++) {
       let li = document.createElement("li");
@@ -14,4 +15,21 @@ window.addEventListener("DOMContentLoaded", () => {
       document.getElementById("list").appendChild(li);
     }
   });
+
+  document.getElementById("showMinimum").addEventListener("click", () => {
+    document.getElementById("minimum").innerHTML = 
+    `
+    A lista minimuma: ${minimum(numberList)}
+    `;
+  });
 });
+
+function minimum(array) {
+  let min = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
+    }
+  }
+  return min;
+}
